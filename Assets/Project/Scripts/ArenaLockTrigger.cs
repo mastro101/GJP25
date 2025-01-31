c6f6b58a6eb382c493d38c3e905bb5616424db92
Matteo <teo.mastropasqua@gmail.com>
2025-01-31 12:53:28 +0100

    HP boss on trigger

--- Assets/Project/Scripts/ArenaLockTrigger.cs
+++ Assets/Project/Scripts/ArenaLockTrigger.cs
@@ -6,18 +6,21 @@ public class ArenaLockTrigger : MonoBehaviour
     [SerializeField] GameObject wall;
     [SerializeField] GameObject boss;
     [SerializeField] AudioSource bossSoundtrack;
+    [SerializeField] GameObject bossHP;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         wall.GetComponent<BoxCollider>().enabled = false;
         boss.GetComponent<BehaviorGraphAgent>().enabled = false;
+        bossHP.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         wall.GetComponent<BoxCollider>().enabled = true;
         boss.GetComponent<BehaviorGraphAgent>().enabled = true;
+        bossHP.SetActive(true);
         bossSoundtrack.Play();
     }
 }
